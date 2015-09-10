878a2fc302f8e38dae6afe277edc4bc72ef6e53a
mrAppleXZ <mr.applexz@gmail.com>
2015-09-10 13:52:44 +0600

    First commit

---
+++ updater.pcs/Main.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace updater.pcs
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.barDownloading = new System.Windows.Forms.ProgressBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.labelDownloading = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // barDownloading
+            // 
+            this.barDownloading.Location = new System.Drawing.Point(2, 41);
+            this.barDownloading.Name = "barDownloading";
+            this.barDownloading.Size = new System.Drawing.Size(357, 29);
+            this.barDownloading.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.barDownloading.TabIndex = 0;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // labelDownloading
+            // 
+            this.labelDownloading.AutoSize = true;
+            this.labelDownloading.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelDownloading.Location = new System.Drawing.Point(12, 9);
+            this.labelDownloading.Name = "labelDownloading";
+            this.labelDownloading.Size = new System.Drawing.Size(79, 29);
+            this.labelDownloading.TabIndex = 1;
+            this.labelDownloading.Text = "label1";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 78);
+            this.Controls.Add(this.labelDownloading);
+            this.Controls.Add(this.barDownloading);
+            this.Name = "Main";
+            this.Text = "Обновление PCS...";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar barDownloading;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label labelDownloading;
+    }
+}
+
---
+++ updater.pcs/Main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace updater.pcs
+{
+    public partial class Main : Form
+    {
+
+        public static WebClient client = new WebClient();
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            client.DownloadProgressChanged += client_DownloadProgressChanged;
+            client.DownloadFileCompleted += client_DownloadFileCompleted;
+        }
+
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            labelDownloading.Text = "Удаление старого файла игры...";
+            File.Delete("pcs.exe");
+            labelDownloading.Text = "Переименовывание нового файла игры...";
+            File.Move("new.pcs.exe", "pcs.exe");
+            labelDownloading.Text = "Все готово!";
+            Process.Start("pcs.exe");
+            Application.Exit();
+        }
+
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            barDownloading.Maximum = (int)e.TotalBytesToReceive / 100;
+            barDownloading.Value = (int)e.BytesReceived / 100;
+            labelDownloading.Text = "Загрузка игры... (" + e.ProgressPercentage + "%)";
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+            client.DownloadFileAsync(new Uri("http://pcs.pearx.ru/pcs.exe"), "new.pcs.exe");
+        }
+    }
+}
